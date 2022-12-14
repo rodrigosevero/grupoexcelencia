a282c6d51feb67cd4e51e1a6bd87ef910a3948b4
Rodrigo Severo <rodrigoseverodev@gmail.com>
2022-12-14 15:20:33 -0400

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-header">
-                    Home1
+                    Home
                 </div>
 
                 <!-- <div class="card-body">
